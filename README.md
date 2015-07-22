5808941e885c3975e68069d5459b7ed29a1d06f8
emmaladouceur <emmakatherine@optonline.net>
2015-07-22 18:58:34 +0000

    working on the function

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Welcome to Battleship! This version works a bit differently: instead of having 5
 
 2. Make it so that the player has 25 shots to start with, and they lose a shot every time they fire. Remove the ability to fire when the player is out of ammo, show the number of shots remaining in the appropriate spot on the webpage.
 
-3. When the "Fire" button is clicked, check if they hit the ship they specified. The row should be input as a number between 0 and 7, and the column as a letter between A and H. If it's a hit, change the emoji to 💣. If it's a miss, change the emoji to 🙅.
+3. When the "Fire" button is clicked, check if they hit the ship they specified. The row should be input as a number between 0 and 7, and the column as a letter between A and H. If it's a hit a ship, change the emoji to 💣. If it's a miss, change the emoji to 🙅.
 
 4. When you win, have the text "You win, click to play again" pop up, and have the page refresh when you click the link. Do the same thing for a loss.
--- js/main.js
+++ js/main.js
@@ -1,5 +1,61 @@
 $(document).ready(function(){
     battleInit();
+ var placementCol = $('#colinput').val();
+ var placementRow = $('#rowinput').val();
+switch(placementCol) {
+    case "a":
+        placementCol = 1;
+        break;
+    case 'b':
+        placementCol = 2;
+        break;
+      case 'c':
+        placementCol = 3;
+        break;
+      case 'd':
+        placementCol = 4;
+        break;
+      case 'e' :
+        placementCol = 5;
+        break;
+      case 'f':
+        placementCol = 6;
+        break;
+      case 'g':
+        placementCol = 7;
+        break;
+      case 'h':
+        placementCol = 8;
+        break;
+    default: 
+        break;
+}
+ var shots = 25;
+ $('#shots_rem').html(shots);
+    $('.shoot').click( 
+      function(){
+          if (board [placementRow - 1][placementCol - 1] == true){
+            shots = shots - 1;
+//        return the new emoji  and replace wave
+           else if (board [placementRow - 1][placementCol - 1] == false){
+           //return the x emoji
+            shots = shots - 1;
+           };
+        };
+      };
+   
+   
+//   var shot = $('#shoot').click(
+//       function(){
+//         shots - 1
+//         return("You have " shot "s left.")
+//         if shots = 0q{(
+//           return("You lose!")
+//         )}
+        
+//       })
+//   var totalShipCount = 9
+//     if
     //Variables you have access to:
     //totalShipCount = number of ships that have been spawned on the board
     //board = 2D array/matrix of boolean values. True means that a ship is in that space, false means that there is no ship.
